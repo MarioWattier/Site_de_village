0e7d48e4cc1070bc7c2ab4e447911be4ad294c9a
Mario Wattier <mario.wattier@protonmail.com>
2020-03-09 14:09:43 +0100

    création de l'index

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController extends AbstractController
+{
+    /**
+     * @Route("/accueil", name="accueil")
+     */
+    public function index()
+    {
+        return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home() {
+        retunr $this->render('accueil/home.html.twig')
+    }
+
+
+}
